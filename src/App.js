7a55b505ba9d266b424b8ca1db9c34b6f760b249
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-09-03 18:47:44 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,1 +1,35 @@
-// create your App component here
+import React from 'react';
+import ExampleComponent from './components/ExampleComponent.js'
+import Greeting from './components/Greeting.js'
+import ButtonComponent from './components/Button.js'
+
+export default class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {peopleInSpace: []}
+  }
+
+  componentDidMount() {
+    const url = "http://api.open-notify.org/astros.json"
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        peopleInSpace: data.people
+      })
+    })
+  }
+
+  render() {
+    let people = this.state.peopleInSpace.map(person => {
+      return <ExampleComponent person={person} key={person.name} />
+    })
+    return (
+      <div>
+        {people}
+      </div>
+    )
+  }
+
+}
--- src/components/ExampleComponent.js
+++ src/components/ExampleComponent.js
@@ -3,13 +3,14 @@ import React, { Component } from 'react';
 class ExampleComponent extends Component {
 
   render() {
+    console.log(this.props)
     return (
-      <strong>
-        Example!
-      </strong>
+      <div>
+        <strong>Name: {this.props.person.name}</strong>
+        <p>Craft: {this.props.person.craft}</p>
+      </div>
     )
   }
-
 }
 
 export default ExampleComponent;
